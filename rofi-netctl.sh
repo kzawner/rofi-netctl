463f6488c4f485c873d46b847a317523c5b803c4
Victor Sobol <vsobol@maven.co>
2018-03-20 21:29:44 +0200

    specify font and -i option

--- rofi-netctl.sh
+++ rofi-netctl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-choice=`find /etc/netctl -maxdepth 1 -type f -printf "%f\n" | rofi -dmenu`;
+choice=`find /etc/netctl -maxdepth 1 -type f -printf "%f\n" \
+        | rofi -font 'Misc Tamsyn,14' -i -dmenu`;
 if [ -n "$choice" ]; then
     gksudo netctl switch-to $choice;
 fi
