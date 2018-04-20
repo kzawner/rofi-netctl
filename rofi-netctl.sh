9a81db1647b6e450aab7e92e4013853fcf430e79
Victor Sobol <vsobol@maven.co>
2018-04-20 10:28:10 +0300

    tmp

--- rofi-netctl.sh
+++ rofi-netctl.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
-choice=`find /etc/netctl -maxdepth 1 -type f -printf "%f\n" \
-        | rofi -font 'Misc Tamsyn,14' -i -dmenu`;
+#
+# Flags:
+#  - a: Choose only from currently active access points;
+#
+
+dev=wlp3s0
+
+profiles=$(find /etc/netctl -maxdepth 1 -type f -printf "%f\n")
+
+if [[ "$1" == "-a" ]]; then
+  active_ap=$(sudo iw dev $dev scan | grep SSID: | gawk '{print $2}' \
+        | xargs -l1 printf "%q|" | sed 's/|$//')
+
+  profiles=$(echo "$profiles" | grep -E $active_ap)
+fi
+
+choice=`echo "$profiles" | rofi -font 'Misc Tamsyn,14' -i -dmenu -p 'network profile'`;
+
 if [ -n "$choice" ]; then
-    gksudo netctl switch-to $choice;
+    sudo netctl switch-to $choice;
 fi
